955d0155eb67c193596988e1b22cb6c456c0a162
Neto <dominguezernesto222@gmail.com>
2025-06-30 14:57:01 -0600

    arreglo conexion1

--- db.js
+++ db.js
@@ -3,12 +3,19 @@ require("dotenv").config();
 
 const isRender = process.env.DB_HOST?.includes("render.com");
 
+console.log("Variables DB:", {
+  DB_USER: process.env.DB_USER,
+  DB_HOST: process.env.DB_HOST,
+  DB_NAME: process.env.DB_NAME,
+  DB_PORT: process.env.DB_PORT,
+});
+
 const pool = new Pool({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  port: Number(process.env.DB_PORT),
   ssl: isRender ? { rejectUnauthorized: false } : false,
 });
 
